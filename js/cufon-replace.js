05ae2763211655864344dad4a75fda1dd4dee361
Dmitry Mishutin <mishutindima@gmail.com>
2014-03-24 22:08:48 +0400

    Удаление проекта с хостинга

--- js/cufon-replace.js
+++
@@ -1,3 +0,0 @@
-/*Cufon.replace('#top_nav a, #splash_menu .button, #menu .text, #icons li, #footer_logo, .submenu_1 a, .font1, .font3, .font_body, .font_body_1, .font_body_2, .font_body_3, .list1 a, .button1', { fontFamily: 'Calibri', hover:true });
-Cufon.replace('h2', { fontFamily: 'Calibri', textShadow:'#d2d2d2 1px 1px, #ececec 2px 2px' });*/
-Cufon.replace('#menu .text', { fontFamily: 'Calibri', hover:true });
